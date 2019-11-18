b8d017c1fc3f6b14859f0029a69faf036a5bdd94
SarahF <sarahfrerichs@gmail.com>
2019-11-17 22:07:58 -0600

    finished class 4 exercises

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,11 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+import java.util.ArrayList;
+
+public class Course {
+    String courseName;
+    String instructor;
+    ArrayList<Student> studentList = new ArrayList<>();
+
+
+}
--- src/org/launchcode/java/demos/lsn4classes2/Student.java
+++ src/org/launchcode/java/demos/lsn4classes2/Student.java
@@ -1,5 +1,7 @@
 package org.launchcode.java.demos.lsn4classes2;
 
+import java.util.Objects;
+
 public class Student {
 
     private static int nextStudentId = 1;
@@ -7,12 +9,14 @@ public class Student {
     private int studentId;
     private int numberOfCredits = 0;
     private double gpa = 0.0;
+    private String gradeLevel;
 
     public Student (String name, int studentId, int numberOfCredits, double gpa) {
         this.name = name;
         this.studentId = studentId;
         this.numberOfCredits = numberOfCredits;
         this.gpa = gpa;
+        this.gradeLevel = gradeLevel;
     }
 
     public Student(String name, int studentId) {
@@ -30,20 +34,67 @@ public class Student {
 
 
      //TODO: Uncomment and complete the getGradeLevel method here:
-//    public String getGradeLevel() {
-//        // Determine the grade level of the student based on numberOfCredits
-//    }
+    public String getGradeLevel() {
+        if (numberOfCredits >=0 && numberOfCredits < 30) {
+            gradeLevel = "Freshman";
+            return gradeLevel;
+        } else if (numberOfCredits >= 30 && numberOfCredits < 60) {
+            gradeLevel = "Sophomore";
+            return gradeLevel;
+        } else if (numberOfCredits >= 60 && numberOfCredits <90) {
+            gradeLevel = "Junior";
+            return gradeLevel;
+        } else if (numberOfCredits >= 90 && numberOfCredits <= 100){
+            gradeLevel = "Senior";
+            return gradeLevel;
+        } else {
+            gradeLevel = "Error!";
+            return gradeLevel;
+        }
+        // Determine the grade level of the student based on numberOfCredits
+    }
 
     // TODO: Complete the addGrade method.
     public void addGrade(int courseCredits, double grade) {
+        double totalQualityScore = gpa*numberOfCredits + grade*courseCredits;
+        numberOfCredits += courseCredits;
+        gpa = totalQualityScore/numberOfCredits;
         // Update the appropriate fields: numberOfCredits, gpa
     }
 
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
     //  than just the class fields.
 
+    @Override
+    public String toString() {
+        return "Student " +
+                "name: " + name +
+                ", studentId:" + studentId +
+                ", numberOfCredits:" + numberOfCredits +
+                ", gpa:" + gpa +
+                ", gradeLevel:" + gradeLevel;
+    }
+
+
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Student objects equal.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return studentId == student.studentId &&
+                numberOfCredits == student.numberOfCredits &&
+                Double.compare(student.gpa, gpa) == 0 &&
+                Objects.equals(name, student.name) &&
+                Objects.equals(gradeLevel, student.gradeLevel);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, studentId, numberOfCredits, gpa, gradeLevel);
+    }
 
     public String getName() {
         return name;
@@ -80,10 +131,13 @@ public class Student {
     public static void main(String[] args) {
         Student sally = new Student("Sally",1,1,4.0);
         System.out.println("The Student class works! " + sally.getName() + " is a student!");
+        sally.getGradeLevel();
         System.out.println(sally);
         sally.addGrade(12, 3.5);
+        sally.getGradeLevel();
         System.out.println(sally);
         sally.addGrade(25, 3.8);
+        sally.getGradeLevel();   
         System.out.println(sally);
     }
 }
